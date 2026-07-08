9c1e02c877f9ae01330b9b8f982f0d337a1b8e9f
Dave Coleman <dave@dvclmn.com>
2026-07-08 08:30:36 +0200

    Renaming of CodableColour to ColourDescriptor. File org

---
+++ Sources/ColourTools/ColourCore/NamedColour.swift
@@ -0,0 +1,12 @@
+//
+//  NamedColour.swift
+//  ToolKit
+//
+//  Created by Dave Coleman on 8/7/2026.
+//
+
+public struct NamedColourAsset {
+  let name: String
+  let source: String
+  let colour: ColourDescriptor
+}
--- Sources/ColourTools/ColourStyle/ColourStyle.swift
+++ Sources/ColourTools/ColourStyle/ColourStyle.swift
@@ -5,8 +5,14 @@
 //  Created by Dave Coleman on 8/7/2026.
 //
 
+// TODO: Can this produce a SwiftUI ShapeStyle?
 public enum ColourStyle {
+
   case solid(RGBColour)
+
+  // TODO: Just linear for now, maybe support more later
   case gradient([RGBColour])
-  case blend(from colour: RGBColour, to: RGBColour, strength: Double)
+
+  /// Returns a solid colour from a blend of two other colours
+  case blend(from: RGBColour, to: RGBColour, strength: Double)
 }
